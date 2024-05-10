cad99fd718ec22fc8992e9010070f0c496a60cd9
Johnson adeola <johnsamxy@gmail.com>
2024-05-10 17:18:11 +0100

    implemeted the express server

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const indexRoute = require('./routes/index');
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+app.use('', indexRoute);
